495b6f5f5f40ddad4859abb42635cf5251f75982
Vineet Kumar <1005vineet.kumar@gmail.com>
2026-02-08 10:45:59 +0530

    Adding adder module

---
+++ adder/full_adder.v
@@ -0,0 +1,29 @@
+//dataflow
+module full_adder(
+	input a, b, c,
+	output s, cout
+);
+	assign s = a ^ b ^ c;
+	assign cout = (a & b) | (b & c) | (c & a); 
+endmodule
+
+
+//structural
+module full_adder(
+	input a, b, c,
+	output s, cout
+);
+	wire s1, c1, c2;
+	half_adder(a, b, s1, c1);
+	half_adder(c, s1, s, c2);
+	
+	assign cout = c1 | c2;
+endmodule
+
+module half_adder(
+	input a, b,
+	output s, c
+);
+	assign s = a ^ b;
+	assign c = a & b;
+endmodule
